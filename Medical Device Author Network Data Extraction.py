a8c2af191aa70c3ce38750cd52e5b16847f5eb8d
BriceB-20 <67841527+BriceB-20@users.noreply.github.com>
2020-07-13 12:17:37 -0400

    Create Medical Device Author Network Data Extraction.py

---
+++ Medical Device Author Network Data Extraction.py
@@ -0,0 +1,98 @@
+import openpyxl
+import re
+import uuid
+
+UUID_dict = {}  # {author name: author ID}
+edges_dict = {}  # {topic identifier: [matching authors id list]}
+cat_dict = {}  # {topic identifier: topic name string}
+paper_author_list = []  # Contains lists of all author for each paper in list form
+
+
+def extract_data(sheet, topic_identifier):
+    for i in range(2, sheet.max_row):
+        cell = "C{}".format(str(i))
+
+        #  Cleans cell text
+        cell_author_string = str(sheet[cell].value)
+        cell_author_string = re.sub("[;]", ",", cell_author_string)
+        cell_author_list = cell_author_string.replace(".", "").split(",")
+
+        paper_author_list.append(cell_author_list)  # Creates a list of all the author names in each cell
+
+        # Further cleans text
+        for new_author in cell_author_list:
+            index_value = cell_author_list.index(new_author)
+            cell_author_list[index_value] = new_author.strip()
+            if cell_author_list[index_value] == "editor" or cell_author_list[index_value] == "et al":
+                del cell_author_list[index_value]
+
+        #  Assigns each value in each list for each cell a unique ID
+        for new_author in cell_author_list:
+            try:
+                x = UUID_dict[new_author]
+                #  repetitions_dict[author] += 1
+            except KeyError:
+                UUID_dict[new_author] = uuid.uuid4().hex  # This can probably be improved to assure no duplicates
+                #  repetitions_dict[author] = 1  # Counts the number of times an author appears in the data
+
+        # Creates category in edges_dict and adds authors. For network analysis in Gephi.
+        try:
+            x = edges_dict[topic_identifier]
+        except KeyError:
+            edges_dict[topic_identifier] = []
+
+        for new_author in cell_author_list:
+            edges_dict[topic_identifier].append(UUID_dict[new_author])
+
+
+# Pacemakers
+workbook = openpyxl.load_workbook(
+    r"pacemakers.xlsx")
+sheet_list = workbook.sheetnames
+pacemakers_sheet = workbook[sheet_list[0]]
+extract_data(sheet=pacemakers_sheet, topic_identifier=1)
+workbook.close()
+
+
+#  Data Output
+data_output = openpyxl.Workbook()
+ID_sheet = data_output["Sheet"]
+ID_sheet.title = "ID"
+# repetitions_sheet = data_output.create_sheet("Repetitions")
+edges_sheet = data_output.create_sheet("Edges")
+
+# Labels
+ID_sheet["A1"] = "Id"
+ID_sheet["B1"] = "Label"
+edges_sheet["A1"] = "Source"
+edges_sheet["B1"] = "Target"
+
+# Produce ID sheet
+count = 2
+for category in edges_dict:
+    a_column = "A{}".format(str(count))
+    b_column = "B{}".format(str(count))
+    ID_sheet[b_column] = ""  # Must fill in manually on spreadsheet
+    ID_sheet[a_column] = category
+    count += 1
+
+for author in UUID_dict:
+    a_column = "A{}".format(str(count))
+    b_column = "B{}".format(str(count))
+    ID_sheet[b_column] = author
+    ID_sheet[a_column] = UUID_dict[author]
+    count += 1
+
+# Produce edges sheet
+count = 2
+for category in edges_dict:
+    for target_author in edges_dict[category]:
+        a_column = "A{}".format(str(count))
+        b_column = "B{}".format(str(count))
+        edges_sheet[a_column] = category
+        edges_sheet[b_column] = target_author
+        count += 1
+
+
+data_output.save(r"C:\Users\Briceno\Desktop\(till_2000 07062020)medicaldev-cat-output.xlsx")
+data_output.close()
