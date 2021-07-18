a335197a48d7a2537c69cb8f96dd47ded1122209
Brice <blbowrey@gmail.com>
2021-07-18 16:20:01 -0400

    JSON update for network graph

--- Modules/process_pubmed_data.py
+++ Modules/process_pubmed_data.py
@@ -1,28 +1,33 @@
 import re
 import json
+import datetime # needed for eval() function
+import pandas as pd
 from fuzzywuzzy import process,fuzz
 
-# Create author info json file from raw pubmed extraction dictionary
-def get_author_info(pubmed_data, cutoff_year, write_file_name):
+
+def get_author_info(pubmed_data_df, cutoff_year, write_file_name):
+    '''
+    Create author info json file from raw pubmed extraction json file
+    '''
     identifier_list = {}
     id_count = 1
-    
+
     # Create data structure for author
-    for i, paper in enumerate(pubmed_data["Authors"]):
+    for i, paper in enumerate(pubmed_data_df["Authors"]):
         for author in paper:
-            if author["name"] != None and eval(pubmed_data["Date"][i]).year <= cutoff_year:
+            if author["name"] != None and eval(pubmed_data_df["Date"][i]).year <= cutoff_year:
                 
                 # If author is already in the dictionary
                 if author["name"].lower() in identifier_list.keys():
 
                     # Update Values
                     identifier_list[author["name"].lower()]["weight"] += 1
-                    identifier_list[author["name"].lower()]["years active"].append(pubmed_data["Date"][i])      
+                    identifier_list[author["name"].lower()]["years active"].append(pubmed_data_df["Date"][i])      
 
                 # If the author is not already in the dictionary
                 else:
                     identifier_list[author["name"].lower()]  = {"id": id_count, 
-                                                        "years active":[pubmed_data["Date"][i]], 
+                                                        "years active":[pubmed_data_df["Date"][i]], 
                                                         "affiliation":[], "weight": 1, "edges":[]}
 
                 # Get year/affiliation data for GIS
@@ -32,22 +37,32 @@ def get_author_info(pubmed_data, cutoff_year, write_file_name):
                     aff = re.sub(r"(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)", "", aff)  # Remove websites
                     aff = aff.strip(" ")
                     if aff != "":  # Skips authors who only put an email as thier insitution
-                        identifier_list[author["name"].lower()]["affiliation"].append((aff, pubmed_data["Date"][i]))
+                        identifier_list[author["name"].lower()]["affiliation"].append((aff, pubmed_data_df["Date"][i]))
 
                 # Append other authors to edges list
-                temp_list = [(o_author["name"].lower(), pubmed_data["Date"][i]) for o_author in paper]  # Co-author name and year of collaboration
+                temp_list = [[o_author["name"].lower(), 0, pubmed_data_df["Date"][i]] for o_author in paper]  # Zero holds place for co-author ID
                 for item in temp_list:
                     if item[0] != author["name"].lower():
-                        identifier_list[author["name"].lower()]["edges"].append(item) 
-
+                        identifier_list[author["name"].lower()]["edges"].append(item)
+                
                 id_count +=1
+                
+    # Get author id for co-authors
+    for author in identifier_list:
+        for item in identifier_list[author]["edges"]:
+            if item[0] in identifier_list.keys():
+                item[1] = identifier_list[item[0]]["id"]
+
             
     with open(write_file_name, "w") as f:
         json.dump(identifier_list, f, indent=4)
 
 
-# Clean author insitituitonal affiliations using fuzzy matching
+
 def match_affiliation(author_data, min_ratio, write_file_name):
+    '''
+    Clean author insitituitonal affiliations using fuzzy matching
+    '''
     full_list = [] 
     for author in author_data: # Extract all listed institutions
         for item in author_data[author]["affiliation"]:
